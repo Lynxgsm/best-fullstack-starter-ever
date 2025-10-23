379631457fea4b06e34a1bf6effcf75bc6a1a058
Aili Fida Aliotti Christino <aili.fida.ac@gmail.com>
2025-10-24 01:17:29 +0200

    feat(web): enhance application with new components and styling
    - Added new UI components including Badge and Button with variant support.
    - Introduced configuration files for Biome and component settings.
    - Updated package.json to include new dependencies such as class-variance-authority and tw-animate-css.
    - Enhanced App component to display categories using the new Badge component.
    - Expanded CSS styles with Tailwind and custom themes for improved design consistency.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,23 @@
 import { useAllCategoriesQuery } from '@bfse/server'
+import { Badge } from '@/components/ui/badge';
 
 export default function App() {
   const { data, loading, error } = useAllCategoriesQuery();
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  console.log(data);
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className='text-2xl font-bold'>Hello World</h1>
+      <ul className='flex flex-wrap gap-2'>
+        {data?.allCategories?.nodes.map((category) => (
+          <li key={category?.id}>
+            <Badge variant='outline'>
+              {category?.name}
+            </Badge>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
